fd60535ad2dac3313c20c14ba0bcf338ba0bce0f
Andrii Krenevych <krenevych@knu.ua>
2026-09-18 11:53:21 +0300

    Start first activity.

--- app/src/main/java/com/example/fistapplication/SecondActivity.kt
+++ app/src/main/java/com/example/fistapplication/SecondActivity.kt
@@ -1,11 +1,27 @@
 package com.example.fistapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fistapplication.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+
+        // Ініціалізуємо binding для другої активності
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Додаємо обробник натискання для повернення на MainActivity
+        binding.btnRunFirstActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+//             Очищаємо стек, щоб не створювати нескінченну кількість копій MainActivity та SecondActivity
+//            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+        }
     }
 }
