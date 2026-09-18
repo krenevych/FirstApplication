4802f469afac8db081ecfe37f61242387c44c6b9
Andrii Krenevych <krenevych@knu.ua>
2026-09-18 11:02:19 +0300

    Activity as View.OnClickListener

--- app/src/main/java/com/example/fistapplication/MainActivity.kt
+++ app/src/main/java/com/example/fistapplication/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.fistapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val TAG = "XXXX"
+
+    private lateinit var resultTextView: TextView
+    private lateinit var inputEditText: EditText
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,26 +23,28 @@ class MainActivity : AppCompatActivity() {
 
 
         // 2. Знаходимо елементи інтерфейсу за їхніми ID з XML файлу
-        val resultTextView: TextView = findViewById(R.id.resultTextView)
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        resultTextView = findViewById(R.id.resultTextView)
+        inputEditText = findViewById(R.id.inputEditText)
+
         val actionButton = findViewById<Button>(R.id.actionButton)
 
-        actionButton.setOnClickListener {
+        actionButton.setOnClickListener(this)
 
-            // Отримуємо текст, який ввів користувач в EditText
-            val inputText = inputEditText.text
+    }
 
-            // Перевіряємо, чи введений текст не порожній
-            if (inputText.isNotBlank()) {
-                // Змінюємо властивість .text у TextView
-                resultTextView.text = "Привіт, $inputText!"
+    override fun onClick(view: View?) {
+        // Отримуємо текст, який ввів користувач в EditText
+        val inputText = inputEditText.text
 
-                // (Опційно) Очистити поле після натискання
-                // inputEditText.text.clear()
-            } else {
-                resultTextView.text = "Будь ласка, введіть ім'я."
-            }
+        // Перевіряємо, чи введений текст не порожній
+        if (inputText.isNotBlank()) {
+            // Змінюємо властивість .text у TextView
+            resultTextView.text = "Привіт, $inputText!"
 
+            // (Опційно) Очистити поле після натискання
+            // inputEditText.text.clear()
+        } else {
+            resultTextView.text = "Будь ласка, введіть ім'я."
         }
     }
 }
