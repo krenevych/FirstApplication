93cc7afde437df751b62b2b5bc112366cb952b80
Andrii Krenevych <krenevych@knu.ua>
2026-09-18 11:50:03 +0300

    Start second activity.

--- app/src/main/java/com/example/fistapplication/MainActivity.kt
+++ app/src/main/java/com/example/fistapplication/MainActivity.kt
@@ -1,41 +1,25 @@
 package com.example.fistapplication
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fistapplication.databinding.ActivityFirstBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFirstBinding
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 2. Ініціалізуємо binding, "надуваючи" (inflating) XML-розмітку
+        // Ініціалізуємо binding, "надуваючи" (inflating) XML-розмітку
         binding = ActivityFirstBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.actionButton.setOnClickListener(this)
-
-    }
-
-    override fun onClick(view: View?) = with(binding){
-        // Отримуємо текст, який ввів користувач в EditText
-        val inputText = inputEditText.text
-
-        // Перевіряємо, чи введений текст не порожній
-        if (inputText.isNotBlank()) {
-            // Змінюємо властивість .text у TextView
-            resultTextView.text = "Привіт, $inputText!"
-
-            // (Опційно) Очистити поле після натискання
-            // inputEditText.text.clear()
-        } else {
-            resultTextView.text = "Будь ласка, введіть ім'я."
+        // Додаємо обробник натискання для переходу на SecondActivity
+        binding.btnRunSecondActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
