7289e7f6303ae148651c6258f1bda3e9b27e4f32
Andrii Krenevych <krenevych@knu.ua>
2026-09-25 09:51:42 +0300

    LifecycleEventObservers

--- app/src/main/java/com/example/fistapplication/MyApplication.kt
+++ app/src/main/java/com/example/fistapplication/MyApplication.kt
@@ -1,16 +1,64 @@
 package com.example.fistapplication
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import android.util.Log
 
 class MyApplication : Application(){
 
     val TAG = "XXX"
 
+
+
+
+
     override fun onCreate() {
         super.onCreate()
 
         Log.d(TAG, "MyApplication: Застосунок стартував! Тут можна ініціалізувати бібліотеки.")
+
+//        registerActivityLifecycleCallbacks(ActivityObserver())
+
+
+        registerActivityLifecycleCallbacks(
+            object : ActivityLifecycleCallbacks {
+                override fun onActivityCreated(
+                    activity: Activity,
+                    savedInstanceState: Bundle?
+                ) {
+                    Log.d(TAG, "onActivityCreated: $activity")
+                }
+
+                override fun onActivityDestroyed(activity: Activity) {
+                    Log.d(TAG, "onActivityDestroyed: $activity")
+                }
+
+                override fun onActivityPaused(activity: Activity) {
+                    Log.d(TAG, "onActivityPaused: $activity")
+                }
+
+                override fun onActivityResumed(activity: Activity) {
+                    Log.d(TAG, "onActivityResumed: $activity")
+                }
+
+                override fun onActivitySaveInstanceState(
+                    activity: Activity,
+                    outState: Bundle
+                ) {
+                    Log.d(TAG, "onActivitySaveInstanceState: $activity")
+                }
+
+                override fun onActivityStarted(activity: Activity) {
+                    Log.d(TAG, "onActivityStarted: $activity")
+                }
+
+                override fun onActivityStopped(activity: Activity) {
+                    Log.d(TAG, "onActivityStopped: $activity")
+                }
+
+            }
+        )
     }
 
 
